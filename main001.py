bbee03a8a761cc538f383ecfef4bdb09fe19862e
awcTSC8VgRr7cEJpgRN3-local <90751388+awcTSC8VgRr7cEJpgRN3@users.noreply.github.com>
2022-11-24 00:04:03 +0800

    Use PIL module instead of magic header.

--- main001.py
+++ main001.py
@@ -2,11 +2,12 @@
 
 import os
 import math
+from PIL import Image
 
 def traversal_error_handler(oserror_instance):
     print(oserror_instance) # May do something more substantial here
 
-# png meta should not be coupled with main function
+# PNG meta should not be coupled with main function
 bits_per_byte = 8
 hexstr_png_header = '89504e470d0a1a0a'
 sizeof_png_header = math.ceil(len(hexstr_png_header)*4/bits_per_byte)
@@ -14,8 +15,10 @@ sizeof_png_header = math.ceil(len(hexstr_png_header)*4/bits_per_byte)
 for dirpath, dirnames, filenames in os.walk(os.getcwd(), topdown=True, onerror=traversal_error_handler, followlinks=True):
     print('Traversal path:', dirpath)
     for filename in filenames:
-        with open(os.path.join(dirpath, filename), 'rb') as f:
-            if f.read(sizeof_png_header) == bytes.fromhex(hexstr_png_header): # png meta...
-                print('\t', filename, 'is a png file.')
-            else:
-                print('\t', filename, 'is not a png file.')
+        try:
+            with Image.open(os.path.join(dirpath, filename)) as img: # PNG meta...
+                print('\tFormat of', '"' + filename + '"', 'is', img.format + '.')
+        except IOError as e:
+            print('\t' + str(type(e)), 'is raised by opening', '"' + filename + '".')
+        except Exception as e:
+            print('\t' + str(type(e)) + ':', e) # Other errors...
