0bcc0cfd49e8ef4d4065a172ae2bf7e4b0d127aa
awcTSC8VgRr7cEJpgRN3-local <90751388+awcTSC8VgRr7cEJpgRN3@users.noreply.github.com>
2022-11-24 00:22:45 +0800

    Rename the file extension depending on its format.

--- main001.py
+++ main001.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os
-import file_parser_vc
+import file_parser_vc as fp
 
 def traversal_error_handler(oserror_instance):
-    print(oserror_instance) # May do something more substantial here
+    print(oserror_instance)
+    # May do something more substantial here
 
 for dirpath, dirnames, filenames in os.walk(os.getcwd(), topdown=True, onerror=traversal_error_handler, followlinks=True):
-    print('Traversal path:', dirpath)
+    # print('Traversal path:', dirpath)
     for filename in filenames:
         try:
-            file = file_parser_vc.File(os.path.join(dirpath, filename))
-            if file.isPNG():
-                print('\t"' + filename + '"', 'is PNG.')
-            else:
-                print('\t"' + filename + '"', 'is not PNG.')
+            filepath = os.path.join(dirpath, filename)
+            file = fp.File(filepath)
+            fileext = file.format_ext()
+            if fileext and os.path.splitext(filepath)[1].lower() not in fileext:
+                os.rename(filepath, filepath + fileext[0])
         except Exception as e:
-            print('\t' + str(type(e)) + ':', e) # Other errors...
+            # Other errors...
+            print('\t' + str(type(e)) + ':', e)
